3aacd428b91b432b52b6478285a38a0103a64ef1
neko2048 <show.999991@gmail.com>
2022-11-11 11:45:44 +0800

    update parameters

---
+++ RRtrack/irtParametersRetrieve.py
@@ -0,0 +1,54 @@
+import netCDF4 as nc
+import numpy as np
+import os
+
+class ParamRetriever:
+    def __init__(self, dataDir):
+        self.dataDir = dataDir
+        self.data = nc.Dataset(self.dataDir)
+
+    def printDomainSize(self):
+        print("domainsize_x = {}".format(len(self.data["xc"])))
+        print("domainsize_y = {}".format(len(self.data["yc"])))
+
+    def printlLonLatGrid(self):
+        try:
+            self.data["lon"]
+            print("llonlatgrid = .TRUE. ! suggest .FALSE. when using VVM")
+        except IndexError:
+            print("llonlatgrid = .FALSE.")
+
+    def printUnitArea(self):
+        x0, x1 = self.data["xc"][0], self.data["xc"][1]
+        y0, y1 = self.data["yc"][0], self.data["yc"][1]
+        xyArea = (x1 - x0) * (y1 - y0) / 1e6 # km ^ 2
+        print("unit_area = {}".format(xyArea))
+
+    def printTimeSteps(self):
+        dirPath = "/".join(self.dataDir.split("/")[:-1])
+        files = [entry for entry in os.listdir(dirPath) if os.path.isfile(os.path.join(dirPath, entry))]
+        print("time_steps = {}".format(len([file for file in files if "Thermodynamic" in file])))
+
+    def printLatFirst(self):
+        print("lat_first = {}".format(np.min(self.data["lat"])))
+
+    def printLatIncAndLonInc(self):
+        try: 
+            print("lat_inc = {}".format((self.data["lat"][1:] - self.data["lat"][:-1])[:3]))
+            print("lon_inc = {}".format((self.data["lon"][1:] - self.data["lon"][:-1])[:3]))
+        except IndexError:
+            print("lat_inc and lon_inc: NO LAT/LON info.")
+
+    def printParams(self):
+        self.printDomainSize()
+        self.printlLonLatGrid()
+        self.printUnitArea()
+        self.printTimeSteps()
+        self.printLatFirst()
+        self.printLatIncAndLonInc()
+
+if __name__ == "__main__":
+    #dataDir = "/home/atmenu10246/transition/dat/diurnal_prescribed/archive/exp.L.Thermodynamic-000000.nc"
+    dataDir = "/home/atmenu10246/transition/dat/mjo/archive/mjo_std_mg.L.Thermodynamic-000000.nc"
+    paramRetriever = ParamRetriever(dataDir)
+    paramRetriever.printParams()
--- RRtrack/irt_parameters_fskao.f90
+++ RRtrack/irt_parameters_fskao.f90
@@ -4,12 +4,12 @@ MODULE irt_parameters
 INTEGER, PARAMETER    :: domainsize_x = 1024
 INTEGER, PARAMETER    :: domainsize_y = 1024
 
-LOGICAL, PARAMETER    :: llonlatgrid = .FALSE.
-REAL, PARAMETER       :: unit_area = 0.25 ! in 0.1x0.1 deg grid boxes
-! only used if llonlatgrid=.TRUE., otherwise set to arbitrary value:
-REAL, PARAMETER       :: lat_first = -45.
-REAL, PARAMETER       :: lat_inc = 0.1
-REAL, PARAMETER       :: lon_inc = 0.1
+LOGICAL, PARAMETER    :: llonlatgrid = .TRUE.
+REAL, PARAMETER       :: unit_area = 0.01 ! in 0.1x0.1 deg grid boxes
+! only used if llonlatgrid=.TRUE., otherwvim ise set to arbitrary value:
+REAL, PARAMETER       :: lat_first = -0.4600752153993973
+REAL, PARAMETER       :: lat_inc = 0.00089946
+REAL, PARAMETER       :: lon_inc = 0.00089946
 
 LOGICAL, PARAMETER    :: lperiodic_x = .TRUE.
 LOGICAL, PARAMETER    :: lperiodic_y = .TRUE.
@@ -17,7 +17,7 @@ LOGICAL, PARAMETER    :: lperiodic_y = .TRUE.
 INTEGER, PARAMETER    :: n_fields = 0   ! number of additional averaging fields
 
 ! bins of coarse velocity field
-INTEGER, PARAMETER    :: time_steps = 145    ! total number of timesteps
+INTEGER, PARAMETER    :: time_steps = 628    ! total number of timesteps
 INTEGER, PARAMETER    :: nt_bins = 1         ! 6 hourly
 INTEGER, PARAMETER    :: nx_bins = 1
 INTEGER, PARAMETER    :: ny_bins = 1
@@ -27,7 +27,7 @@ REAL, PARAMETER       :: minimum_size = 1       ! events smaller than that will 
 
 REAL, PARAMETER       :: termination_sensitivity=1.0      ! Choose value between 0.0 and 1.0
 
-REAL, PARAMETER       :: max_velocity = 10.   ! adjust acordingly
+REAL, PARAMETER       :: max_velocity = 15.   ! adjust acordingly
                                               ! velocities>max_velocity will be ignored to remove outliers
 ! define a minimal number of cells required for a coarse grained coordinate to be evaluated 
 ! if there are less, missing value will be assigned to that coarse cell
